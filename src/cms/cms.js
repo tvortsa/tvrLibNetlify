7c2178fab0c9f4bcbd6d5f975e2f8b10a47b8ec6
tvortsa <tvortsa@gmail.com>
2018-06-16 23:15:15 +0300

    Добавил регистрацию article в cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,9 +2,11 @@ import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
+import ArticlePostPreview from './preview-templates/ArticlePostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('article', ArticlePostPreview)
